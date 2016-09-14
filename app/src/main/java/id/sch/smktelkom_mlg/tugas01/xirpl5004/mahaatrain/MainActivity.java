22c55b1a7266a5acef01c8cbf010a56d609e6e0c
anggyanisa <anggyanisa5@gmail.com>
2016-09-14 14:31:46 +0700

    tiga

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5004/mahaatrain/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5004/mahaatrain/MainActivity.java
@@ -45,6 +45,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 doProcess();
             }
+
+            private void doProcess() {
+                if (isValid()) {
+                    String nama1 = etnama.getText().toString();
+                    String date1 = etdate.getText().toString();
+                    String departure1 = sberangkat.getSelectedItem().toString();
+                    String arrival1 = spinner2.getSelectedItem().toString();
+                    String hasil1 = "Makanan yang di pilih adalah : \n";
+                    int startlen = hasil1.length();
+                    if (ind.isChecked()) hasil1 += ind.getText() + "\n";
+                    if (west.isChecked()) hasil1 += west.getText() + "\n";
+                    if (kra.isChecked()) hasil1 += kra.getText() + "\n";
+                    if (mid.isChecked()) hasil1 += mid.getText() + "\n";
+                    if (hasil1.length() == startlen) hasil1 += "Anda Memilih Makanan Biasa";
+                    String hasil2 = null;
+                    if (eco.isChecked()) {
+                        hasil2 = eco.getText().toString();
+                    } else if (ecopre.isChecked()) {
+                        hasil2 = ecopre.getText().toString();
+                    } else if (buss.isChecked()) {
+                        hasil2 = buss.getText().toString();
+                    } else if (first.isChecked()) {
+                        hasil2 = first.getText().toString();
+                    }
+                    tvhasil2.setText("Detail Pemesanan Tiket");
+                    tvhasil.setText("Nama Pemesan : \n" + nama1 + "\n\nTanggal Berangkat : \n" + date1 + "\n\nBandara Keberangkatan Anda : " + "\n" + departure1 + "\n\nStasiun Tujuan Anda : " + "\n" + arrival1 + "\n\n" + hasil1 + "\nKelas Anda : " + "\n" + hasil2);
+
+                }
+            }
         });
 
     }
